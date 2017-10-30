7926933597d702af42adbf193ae97b965e470751
Nico Di Rocco <dirocco.nico@gmail.com>
2017-10-30 13:52:21 +0100

    Add NullInt64 type

--- types.go
+++ types.go
@@ -27,3 +27,26 @@ func (n *NullString) UnmarshalJSON(b []byte) error {
 	}
 	return n.Scan(s)
 }
+
+var nullInt64 = []byte("null")
+
+type NullInt64 struct {
+	sql.NullInt64
+}
+
+// MarshalJSON correctly serializes a NullInt64 to JSON
+func (n NullInt64) MarshalJSON() ([]byte, error) {
+	if n.Valid {
+		return json.Marshal(n.Int64)
+	}
+	return nullInt64, nil
+}
+
+// UnmarshalJSON correctly deserializes a NullInt64 from JSON
+func (n *NullInt64) UnmarshalJSON(b []byte) error {
+	var s interface{}
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+	return n.Scan(s)
+}
